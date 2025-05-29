c18cae92b9c6afc9f479598562dd754c70ca27f0
AugustoAlmarcha <augustoalmarcha@gmail.com>
2025-05-29 20:36:04 -0300

    Add files via upload

---
+++ Trabajo Practico Unidad 5 Augusto Almarcha Completo/config.py
@@ -0,0 +1,8 @@
+
+SECRET_KEY = "GDtfDCFYjD"   #Esta variable SECRET_KEY se utiliza en Flask para proteger tu aplicación contra ataques CSRF 
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/Usuario/Desktop/Python/Trabajo Practico Unidad 5 Augusto Almarcha Completo/datos.sqlite3'  
+#Esta configuración SQLALCHEMY_DATABASE_URI especifica la ubicación de la base de datos
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False 
+#Esta configuración controla si SQLAlchemy debe rastrear las modificaciones en los objetos y enviar señales para la aplicación Flask cada vez que se modifica un objeto.
---
+++ Trabajo Practico Unidad 5 Augusto Almarcha Completo/db.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  #creamos una instancia de SQLAlchemy para usarlo con Flask.
